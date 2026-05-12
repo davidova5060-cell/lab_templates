7f3b5b21e6737fcbe05fd2fb2abcdc18a4e03ab5
davidova5060-cell <davidova5060@gmail.com>
2026-05-12 18:59:56 +0300

    Update array.h

--- array.h
+++ array.h
@@ -1,57 +1,137 @@
 #pragma once
 
 #include <cstddef>
+#include <algorithm>
 
 template <typename T>
 class Array {
 public:
-    // ======================== Constructors ========================
+    // ==================== Constructors ====================
 
-    Array();                                // empty array, size 0
-    explicit Array(size_t size);            // default-constructed elements
-    Array(size_t size, const T& value);     // all elements = value
+    Array() : data_(nullptr), size_(0) {}
 
-    // =================== Copy and Move ===========================
+    explicit Array(size_t size) : data_(new T[size]), size_(size) {}
 
-    Array(const Array& other);
-    Array(Array&& other);
-    Array& operator=(const Array& other);
-    Array& operator=(Array&& other);
+    Array(size_t size, const T& value) : data_(new T[size]), size_(size) {
+        for (size_t i = 0; i < size_; ++i) {
+            data_[i] = value;
+        }
+    }
 
-    // =================== Destructor ==============================
+    // ================= Copy and Move ===================
 
-    ~Array();
+    Array(const Array& other) : data_(new T[other.size_]), size_(other.size_) {
+        for (size_t i = 0; i < size_; ++i) {
+            data_[i] = other.data_[i];
+        }
+    }
 
-    // =================== Element access ==========================
+    Array(Array&& other) noexcept : data_(other.data_), size_(other.size_) {
+        other.data_ = nullptr;
+        other.size_ = 0;
+    }
 
-    T&       operator[](size_t index);
-    const T& operator[](size_t index) const;
+    Array& operator=(const Array& other) {
+        if (this != &other) {
+            delete[] data_;
+            size_ = other.size_;
+            data_ = new T[size_];
+            for (size_t i = 0; i < size_; ++i) {
+                data_[i] = other.data_[i];
+            }
+        }
+        return *this;
+    }
 
-    T&       front();
-    const T& front() const;
+    Array& operator=(Array&& other) noexcept {
+        if (this != &other) {
+            delete[] data_;
+            data_ = other.data_;
+            size_ = other.size_;
+            other.data_ = nullptr;
+            other.size_ = 0;
+        }
+        return *this;
+    }
 
-    T&       back();
-    const T& back() const;
+    // ================= Destructor ===================
 
-    T*       data();
-    const T* data() const;
+    ~Array() {
+        delete[] data_;
+    }
 
-    // =================== Capacity ================================
+    // ================= Element access ===================
 
-    size_t size() const;
-    bool   empty() const;
+    T& operator[](size_t index) {
+        return data_[index];
+    }
 
-    // =================== Operations ==============================
+    const T& operator[](size_t index) const {
+        return data_[index];
+    }
 
-    void fill(const T& value);
-    void swap(Array& other);
+    T& front() {
+        return data_[0];
+    }
 
-    // =================== Comparison ==============================
+    const T& front() const {
+        return data_[0];
+    }
 
-    bool operator==(const Array& rhs) const;
-    bool operator!=(const Array& rhs) const;
+    T& back() {
+        return data_[size_ - 1];
+    }
+
+    const T& back() const {
+        return data_[size_ - 1];
+    }
+
+    T* data() {
+        return data_;
+    }
+
+    const T* data() const {
+        return data_;
+    }
+
+    // ================= Capacity ===================
+
+    size_t size() const {
+        return size_;
+    }
+
+    bool empty() const {
+        return size_ == 0;
+    }
+
+    // ================= Operations ===================
+
+    void fill(const T& value) {
+        for (size_t i = 0; i < size_; ++i) {
+            data_[i] = value;
+        }
+    }
+
+    void swap(Array& other) noexcept {
+        std::swap(data_, other.data_);
+        std::swap(size_, other.size_);
+    }
+
+    // ================= Comparison ===================
+
+    bool operator==(const Array& rhs) const {
+        if (size_ != rhs.size_) return false;
+        for (size_t i = 0; i < size_; ++i) {
+            if (data_[i] != rhs.data_[i]) return false;
+        }
+        return true;
+    }
+
+    bool operator!=(const Array& rhs) const {
+        return !(*this == rhs);
+    }
 
 private:
-    T*     data_ = nullptr;
-    size_t size_ = 0;
+    T* data_;
+    size_t size_;
 };
