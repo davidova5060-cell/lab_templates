65242933eeb2a4687896eb57f681a6ec7a80b8b8
davidova5060-cell <davidova5060@gmail.com>
2026-05-12 19:29:29 +0300

    Update optional.h

--- optional.h
+++ optional.h
@@ -4,110 +4,96 @@
 #include <utility>
 
 template <typename T>
-class Optional
-{
+class Optional {
 public:
-    Optional() : has_value(false) {}
-    Optional(const T& value) : has_value(true), val(value) {}
-    Optional(T&& value) : has_value(true), val(std::move(value)) {}
-    Optional(std::nullopt_t) : has_value(false) {}
+    Optional() : m_has_value(false) {}
+    Optional(const T& value) : m_has_value(true), m_value(value) {}
+    Optional(T&& value) : m_has_value(true), m_value(std::move(value)) {}
+    Optional(std::nullopt_t) : m_has_value(false) {}
 
-    Optional(const Optional& other) : has_value(other.has_value)
-    {
-        if (has_value) val = other.val;
+    Optional(const Optional& other) : m_has_value(other.m_has_value) {
+        if (m_has_value) m_value = other.m_value;
     }
 
-    Optional(Optional&& other) noexcept : has_value(other.has_value)
-    {
-        if (has_value) val = std::move(other.val);
-        other.has_value = false;
+    Optional(Optional&& other) noexcept : m_has_value(other.m_has_value) {
+        if (m_has_value) m_value = std::move(other.m_value);
     }
 
-    Optional& operator=(const Optional& other)
-    {
-        if (this != &other)
-        {
-            has_value = other.has_value;
-            if (has_value) val = other.val;
+    Optional& operator=(const Optional& other) {
+        if (this != &other) {
+            m_has_value = other.m_has_value;
+            if (m_has_value) m_value = other.m_value;
         }
         return *this;
     }
 
-    Optional& operator=(Optional&& other) noexcept
-    {
-        if (this != &other)
-        {
-            has_value = other.has_value;
-            if (has_value) val = std::move(other.val);
-            other.has_value = false;
+    Optional& operator=(Optional&& other) noexcept {
+        if (this != &other) {
+            m_has_value = other.m_has_value;
+            if (m_has_value) m_value = std::move(other.m_value);
         }
         return *this;
     }
 
     ~Optional() = default;
 
-    T* operator->() { return &val; }
-    const T* operator->() const { return &val; }
-    T& operator*() { return val; }
-    const T& operator*() const { return val; }
+    T* operator->() { return &m_value; }
+    const T* operator->() const { return &m_value; }
+    T& operator*() { return m_value; }
+    const T& operator*() const { return m_value; }
 
-    explicit operator bool() const { return has_value; }
-    bool has_value() const { return has_value; }
+    explicit operator bool() const { return m_has_value; }
+    bool has_value() const { return m_has_value; }
 
-    T& value()
-    {
-        if (!has_value) throw std::bad_optional_access();
-        return val;
+    T& value() {
+        if (!m_has_value) throw std::bad_optional_access();
+        return m_value;
     }
 
-    const T& value() const
-    {
-        if (!has_value) throw std::bad_optional_access();
-        return val;
+    const T& value() const {
+        if (!m_has_value) throw std::bad_optional_access();
+        return m_value;
     }
 
-    T value_or(const T& default_value) const
-    {
-        return has_value ? val : default_value;
+    T value_or(const T& default_value) const {
+        return m_has_value ? m_value : default_value;
     }
 
-    void reset()
-    {
-        has_value = false;
+    void reset() {
+        m_has_value = false;
     }
 
-    void swap(Optional& other)
-    {
-        std::swap(has_value, other.has_value);
-        if (has_value && other.has_value)
-            std::swap(val, other.val);
-        else if (has_value)
-            other.val = std::move(val);
-        else if (other.has_value)
-            val = std::move(other.val);
+    void swap(Optional& other) {
+        std::swap(m_has_value, other.m_has_value);
+        if (m_has_value && other.m_has_value) {
+            std::swap(m_value, other.m_value);
+        } else if (m_has_value) {
+            other.m_value = std::move(m_value);
+            reset();
+        } else if (other.m_has_value) {
+            m_value = std::move(other.m_value);
+            other.reset();
+        }
     }
 
     template <typename... Args>
-    T& emplace(Args&&... args)
-    {
-        val = T(std::forward<Args>(args)...);
-        has_value = true;
-        return val;
+    T& emplace(Args&&... args) {
+        m_value = T(std::forward<Args>(args)...);
+        m_has_value = true;
+        return m_value;
     }
 
-    bool operator==(const Optional& rhs) const
-    {
-        if (has_value != rhs.has_value) return false;
-        if (!has_value) return true;
-        return val == rhs.val;
+    bool operator==(const Optional& rhs) const {
+        if (m_has_value != rhs.m_has_value) return false;
+        if (!m_has_value) return true;
+        return m_value == rhs.m_value;
     }
 
-    bool operator!=(const Optional& rhs) const
-    {
+    bool operator!=(const Optional& rhs) const {
         return !(*this == rhs);
     }
 
 private:
-    bool has_value;
-    T val;
+    bool m_has_value;
+    T m_value;
 };
