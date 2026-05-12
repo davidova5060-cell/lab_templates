e9bc4b0b69a3606cc07cfb836410c8313ffed33d
davidova5060-cell <davidova5060@gmail.com>
2026-05-12 19:23:25 +0300

    Update optional.h

--- optional.h
+++ optional.h
@@ -1,106 +1,113 @@
 #pragma once
 
+#include <stdexcept>
 #include <utility>
-#include <memory>
 
 template <typename T>
-class UniquePtr {
+class Optional
+{
 public:
-    UniquePtr() : ptr_(nullptr) {}
-    explicit UniquePtr(T* ptr) : ptr_(ptr) {}
-    UniquePtr(std::nullptr_t) : ptr_(nullptr) {}
+    Optional() : has_value(false) {}
+    Optional(const T& value) : has_value(true), val(value) {}
+    Optional(T&& value) : has_value(true), val(std::move(value)) {}
+    Optional(std::nullopt_t) : has_value(false) {}
 
-    UniquePtr(const UniquePtr&) = delete;
-    UniquePtr& operator=(const UniquePtr&) = delete;
-
-    UniquePtr(UniquePtr&& other) noexcept : ptr_(other.ptr_) {
-        other.ptr_ = nullptr;
+    Optional(const Optional& other) : has_value(other.has_value)
+    {
+        if (has_value) val = other.val;
     }
 
-    UniquePtr& operator=(UniquePtr&& other) noexcept {
-        if (this != &other) {
-            delete ptr_;
-            ptr_ = other.ptr_;
-            other.ptr_ = nullptr;
+    Optional(Optional&& other) noexcept : has_value(other.has_value)
+    {
+        if (has_value) val = std::move(other.val);
+        other.has_value = false;
+    }
+
+    Optional& operator=(const Optional& other)
+    {
+        if (this != &other)
+        {
+            has_value = other.has_value;
+            if (has_value) val = other.val;
         }
         return *this;
     }
 
-    ~UniquePtr() {
-        delete ptr_;
-    }
-
-    T& operator*() const { return *ptr_; }
-    T* operator->() const { return ptr_; }
-    T* get() const { return ptr_; }
-    explicit operator bool() const { return ptr_ != nullptr; }
-
-    T* release() {
-        T* temp = ptr_;
-        ptr_ = nullptr;
-        return temp;
-    }
-
-    void reset(T* new_ptr = nullptr) {
-        delete ptr_;
-        ptr_ = new_ptr;
-    }
-
-    void swap(UniquePtr& other) noexcept {
-        std::swap(ptr_, other.ptr_);
-    }
-
-private:
-    T* ptr_;
-};
-
-// Специализация для массивов
-template <typename T>
-class UniquePtr<T[]> {
-public:
-    UniquePtr() : ptr_(nullptr) {}
-    explicit UniquePtr(T* ptr) : ptr_(ptr) {}
-    UniquePtr(std::nullptr_t) : ptr_(nullptr) {}
-
-    UniquePtr(const UniquePtr&) = delete;
-    UniquePtr& operator=(const UniquePtr&) = delete;
-
-    UniquePtr(UniquePtr&& other) noexcept : ptr_(other.ptr_) {
-        other.ptr_ = nullptr;
-    }
-
-    UniquePtr& operator=(UniquePtr&& other) noexcept {
-        if (this != &other) {
-            delete[] ptr_;
-            ptr_ = other.ptr_;
-            other.ptr_ = nullptr;
+    Optional& operator=(Optional&& other) noexcept
+    {
+        if (this != &other)
+        {
+            has_value = other.has_value;
+            if (has_value) val = std::move(other.val);
+            other.has_value = false;
         }
         return *this;
     }
 
-    ~UniquePtr() {
-        delete[] ptr_;
+    ~Optional() = default;
+
+    T* operator->() { return &val; }
+    const T* operator->() const { return &val; }
+    T& operator*() { return val; }
+    const T& operator*() const { return val; }
+
+    explicit operator bool() const { return has_value; }
+    bool has_value() const { return has_value; }
+
+    T& value()
+    {
+        if (!has_value) throw std::bad_optional_access();
+        return val;
     }
 
-    T& operator[](size_t index) const { return ptr_[index]; }
-    T* get() const { return ptr_; }
-    explicit operator bool() const { return ptr_ != nullptr; }
-
-    T* release() {
-        T* temp = ptr_;
-        ptr_ = nullptr;
-        return temp;
+    const T& value() const
+    {
+        if (!has_value) throw std::bad_optional_access();
+        return val;
     }
 
-    void reset(T* new_ptr = nullptr) {
-        delete[] ptr_;
-        ptr_ = new_ptr;
+    T value_or(const T& default_value) const
+    {
+        return has_value ? val : default_value;
     }
 
-    void swap(UniquePtr& other) noexcept {
-        std::swap(ptr_, other.ptr_);
+    void reset()
+    {
+        has_value = false;
+    }
+
+    void swap(Optional& other)
+    {
+        std::swap(has_value, other.has_value);
+        if (has_value && other.has_value)
+            std::swap(val, other.val);
+        else if (has_value)
+            other.val = std::move(val);
+        else if (other.has_value)
+            val = std::move(other.val);
+    }
+
+    template <typename... Args>
+    T& emplace(Args&&... args)
+    {
+        val = T(std::forward<Args>(args)...);
+        has_value = true;
+        return val;
+    }
+
+    bool operator==(const Optional& rhs) const
+    {
+        if (has_value != rhs.has_value) return false;
+        if (!has_value) return true;
+        return val == rhs.val;
+    }
+
+    bool operator!=(const Optional& rhs) const
+    {
+        return !(*this == rhs);
     }
 
 private:
-    T* ptr_;
+    bool has_value;
+    T val;
 };
